b9fece80ce07ebcc4c852ce55e0a8c601e567ae2
Corentin B <gta447@hotmail.fr>
2023-01-10 15:19:00 +0100

    initial commit

---
+++ src/main/java/com/gk/gestibank/GestiBankApplication.java
@@ -0,0 +1,13 @@
+package com.gk.gestibank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestiBankApplication.class, args);
+	}
+
+}
