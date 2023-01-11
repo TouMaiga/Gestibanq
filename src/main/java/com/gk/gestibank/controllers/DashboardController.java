41e0b1421db50860659306c6f869d69658b74424
TouMaiga <maiga.maimouna@gmail.com>
2023-01-11 17:22:05 +0100

    integration de la maquette front

---
+++ src/main/java/com/gk/gestibank/controllers/DashboardController.java
@@ -0,0 +1,29 @@
+package com.gk.gestibank.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+	
+
+    @GetMapping("/")
+    public String accueil(Model model) {
+        return "dashboard/index";  
+    }
+    
+    @GetMapping("/admin")
+    public String dashbaordAdmin(Model model) {
+        return "dashboard/admin";  
+    }
+    @GetMapping("/agent")
+    public String dashbaordAgent(Model model) {
+        return "dashboard/agent";  
+    }
+    @GetMapping("/client")
+    public String dashbaordAClient(Model model) {
+        return "dashboard/client";  
+    }
+
+}
