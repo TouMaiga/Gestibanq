262d032b7e20fcc0512aa4def26fa80b2789ed7b
zammaliahmed <zammaliahmed.enit@gmail.com>
2023-01-12 12:29:49 +0100

    MAJ Branch

--- src/main/java/com/gk/gestibank/repositories/AgentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gk.gestibank.repositories;
-
-import com.gk.gestibank.entities.Agent;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AgentRepository extends CrudRepository<Agent, Long> {
-
-}
