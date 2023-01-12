f737b24666d7e6c741e51073bb45b5ab0934da47
Corentin B <gta447@hotmail.fr>
2023-01-12 17:52:18 +0100

    file update - to continue

--- src/main/java/com/gk/gestibank/GestiBankApplication.java
+++ src/main/java/com/gk/gestibank/GestiBankApplication.java
@@ -1,13 +1,19 @@
 package com.gk.gestibank;
 
+import java.io.File;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gk.gestibank.controllers.ConfigController;
 
 @SpringBootApplication
 public class GestiBankApplication {
 
 	public static void main(String[] args) {
+		new File(ConfigController.uploadDirectory).mkdir();
 		SpringApplication.run(GestiBankApplication.class, args);
 	}
+	
 
 }
--- src/main/java/com/gk/gestibank/controllers/ConfigController.java
+++ src/main/java/com/gk/gestibank/controllers/ConfigController.java
@@ -1,5 +1,9 @@
 package com.gk.gestibank.controllers;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +14,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.gk.gestibank.entities.Config;
 import com.gk.gestibank.repositories.ConfigRepository;
+
 
 import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/config/")
 public class ConfigController {
+	
+	public static String uploadDirectory =
+			System.getProperty("user.dir")+"/src/main/resources/static/uploads";
 	
 	private final ConfigRepository configRepository;
 	
@@ -57,11 +67,24 @@ public class ConfigController {
 	    }
 	    
 	    @PostMapping("add")
-	    public String addConfig(@Valid Config config, BindingResult result, Model model) {
+	    public String addConfig(@Valid Config config, BindingResult result, Model model, @RequestParam("files") MultipartFile[] files) {
 	        if (result.hasErrors()) {
 	        	System.out.println(result);
 	            return "config/addConfig";
 	        }
+	        
+	      /// part upload
+	        StringBuilder fileName = new StringBuilder();
+	        MultipartFile file = files[0];
+	        Path fileNameAndPath = Paths.get(uploadDirectory,
+	        file.getOriginalFilename());
+	        fileName.append(file.getOriginalFilename());
+	        try {
+	        Files.write(fileNameAndPath, file.getBytes());
+	        } catch (IOException e) {
+	        e.printStackTrace();
+	        }
+	        config.setLogo(fileName.toString());
 	        /*LocalDate ld = LocalDate.now();
 	        Date d = new Date(System.currentTimeMillis());
 	        config.setDate(d);*/
