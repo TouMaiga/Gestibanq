fd3f270594b7eaf9f96bd2a61c243c29f9ed7e35
Corentin B <gta447@hotmail.fr>
2023-01-13 16:05:42 +0100

    added security - to continue

---
+++ src/main/java/com/gk/gestibank/entities/Role.java
@@ -0,0 +1,40 @@
+package com.gk.gestibank.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table; 
+
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private int id;
+    @Column(name = "role")
+    private String role;
+    
+ public Role() {}
+    
+    
+	public Role(String role) {
+		this.role = role;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
+
