0f00857becd28f9606d4af256942ddea6e2aa04b
Corentin B <gta447@hotmail.fr>
2023-01-16 16:32:36 +0100

    fixed bug from index.html

--- src/main/java/com/gk/gestibank/controllers/MessageController.java
+++ src/main/java/com/gk/gestibank/controllers/MessageController.java
@@ -16,90 +16,89 @@ import com.gk.gestibank.repositories.MessageRepository;
 
 import javax.validation.Valid;
 
-        @Controller
-        @RequestMapping("/message/")
+@Controller
+@RequestMapping("/message/")
 public class MessageController {
-	private final MessageRepository messageRepository;
+private final MessageRepository messageRepository;
 
-	@Autowired
-	public MessageController(MessageRepository messageRepository) {
-		this.messageRepository = messageRepository;
+@Autowired
+public MessageController(MessageRepository messageRepository) {
+this.messageRepository = messageRepository;
+}
+
+ @GetMapping("list")
+//@ResponseBody
+public String listMessages(Model model) {
+	
+	List<Message> lp = (List<Message>)messageRepository.findAll();
+	if(lp.size() == 0) {
+		lp = null;
 	}
+    model.addAttribute("messages",lp);	        
+    return "message/listMessage";
+    
+    //List<Provider> lp = (List<Provider>)providerRepository.findAll();
+    //System.out.println(lp);
+    
+    //return "Nombre de fournisseur = " + lp.size();
+}
 
-	     @GetMapping("list")
-	    //@ResponseBody
-	    public String listMessages(Model model) {
-	    	
-	    	List<Message> lp = (List<Message>)messageRepository.findAll();
-	    	if(lp.size() == 0) {
-	    		lp = null;
-	    	}
-	        model.addAttribute("messages",lp);	        
-	        return "message/listMessage";
-	        
-	        //List<Provider> lp = (List<Provider>)providerRepository.findAll();
-	        //System.out.println(lp);
-	        
-	        //return "Nombre de fournisseur = " + lp.size();
-	    }
-	  
-	  
-	  
-		  @GetMapping("add") 
-		  public String showAddMessageForm(Model model) {
-		  Message message = new Message(); 
-		  // object dont la valeur des attributs par defaut
-		  model.addAttribute("message", message); 
-		  return "message/addMessage"; }
-		 
-	 
 
-	@PostMapping("add")
-	public String addMessage(@Valid Message message, BindingResult result, Model model) {
-		if (result.hasErrors()) {
-			return "message/addMessage";
-		}
-		messageRepository.save(message);
-		return "redirect:list";
-	} 
+  @GetMapping("add") 
+  public String showAddMessageForm(Model model) {
+  Message message = new Message() ; 
+  // object dont la valeur des attributs par defaut
+  model.addAttribute("message", message); 
+  return "message/addMessage"; }
+ 
 
-	
-	
-	  @GetMapping("delete/{id}") 
-	  public String deleteMessage(@PathVariable("id") long id, Model model) {
-	  //long id2 = 100L;
-	  
-	  Message message = messageRepository.findById(id) .orElseThrow(()-> new
-	  IllegalArgumentException("Invalid message Id:" + id));
-	  
-	  System.out.println("suite du programme...");
-	  
-	  messageRepository.delete(message);
-	  /* model.addAttribute("providers", messageRepository.findAll()); 
-	  return"message/listMessage"; */
-	  return "redirect:../list";
-	  }
-	  
-	  
-	  
-	  @GetMapping("edit/{id}")
-	  public String showMessageFormToUpdate(@PathVariable("id") long id, Model model) {
-	  Message message = messageRepository.findById(id) .orElseThrow(()->new IllegalArgumentException("Invalid message Id:" + id));
-	  
-	  model.addAttribute("message", message);
-	  
-	  return "message/updateMessage"; 
-	  }
-	  
-	  
-	  
-	  @PostMapping("update") 
-	  public String updateMessage(@Valid Message message, BindingResult result, Model model) {
-	  
-	  messageRepository.save(message); 
-	  return"redirect:list";
-	  
-	  }
-	 
+
+@PostMapping("add")
+public String addMessage(@Valid Message message, BindingResult result, Model model) {
+if (result.hasErrors()) {
+	return "message/addMessage";
+}
+messageRepository.save(message);
+return "redirect:list";
+} 
+
+
+
+@GetMapping("delete/{id}") 
+public String deleteMessage(@PathVariable("id") long id, Model model) {
+//long id2 = 100L;
+
+Message message = messageRepository.findById(id) .orElseThrow(()-> new
+IllegalArgumentException("Invalid message Id:" + id));
+
+System.out.println("suite du programme...");
+
+messageRepository.delete(message);
+/* model.addAttribute("providers", messageRepository.findAll()); 
+return"message/listMessage"; */
+return "redirect:../list";
+}
+
+
+
+@GetMapping("edit/{id}")
+public String showMessageFormToUpdate(@PathVariable("id") long id, Model model) {
+Message message = messageRepository.findById(id) .orElseThrow(()->new IllegalArgumentException("Invalid message Id:" + id));
+
+model.addAttribute("message", message);
+
+return "message/updateMessage"; 
+}
+
+
+
+@PostMapping("update") 
+public String updateMessage(@Valid Message message, BindingResult result, Model model) {
+
+messageRepository.save(message); 
+return"redirect:list";
+
+}
+
 
 }
--- src/main/java/com/gk/gestibank/entities/Message.java
+++ src/main/java/com/gk/gestibank/entities/Message.java
@@ -12,88 +12,124 @@ import javax.validation.constraints.NotBlank;
 @Entity
 
 public class Message {
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private long id;
 
-	@Column(name = "date")
-	private Date date;
+		@Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private long id;
+	    
+		@Column(name = "date")
+	    private Date date ;
+		
+	    @NotBlank(message = "FirstName is mandatory")
+	    @Column(name = "firstname")
+	    private String firstname;
+	    
+	    @NotBlank(message = "LastName is mandatory")
+	    @Column(name = "lastname")
+	    private String lastname;
+	    
+	    @NotBlank(message = "Email is mandatory")
+	    @Column(name = "email")
+	    private String email;
+	    @NotBlank(message = "Sujet is mandatory")
+	    @Column(name = "sujet")
+	    private String sujet;
+	    
+	    @NotBlank(message = "Contenu is mandatory")
+	    @Column(name = "contenu")
+	    private String contenu;
+	    
+	    @Column(name = "reponse")
+	    private String reponse;
 
-	@NotBlank(message = "Sujet is mandatory")
-	@Column(name = "sujet")
-	private String sujet;
 
-	@NotBlank(message = "Auteur is mandatory")
-	@Column(name = "auteur")
-	private String auteur;
+	    public Message() {}
 
-	@NotBlank(message = "Contenu is mandatory")
-	@Column(name = "contenu")
-	private String contenu;
+		public Message(long id, Date date, @NotBlank(message = "FirstName is mandatory") String firstname,
+				@NotBlank(message = "LastName is mandatory") String lastname,
+				@NotBlank(message = "Email is mandatory") String email,
+				@NotBlank(message = "Sujet is mandatory") String sujet,
+				@NotBlank(message = "Contenu is mandatory") String contenu, String reponse) {
+			super();
+			this.id = id;
+			this.date = date;
+			this.firstname = firstname;
+			this.lastname = lastname;
+			this.email = email;
+			this.sujet = sujet;
+			this.contenu = contenu;
+			this.reponse = reponse;
+		}
 
-	@Column(name = "reponse")
-	private String reponse;
 
-	public String getReponse() {
-		return reponse;
-	}
+		public String getSujet() {
+			return sujet;
+		}
 
-	public void setReponse(String reponse) {
-		this.reponse = reponse;
-	}
 
-	public long getId() {
-		return id;
-	}
+		public void setSujet(String sujet) {
+			this.sujet = sujet;
+		}
 
-	public void setId(long id) {
-		this.id = id;
-	}
 
-	public Date getDate() {
-		return date;
-	}
+		public long getId() {
+			return id;
+		}
 
-	public void setDate(Date date) {
-		this.date = date;
-	}
 
-	public String getSujet() {
-		return sujet;
-	}
+		public void setId(long id) {
+			this.id = id;
+		}
 
-	public void setSujet(String sujet) {
-		this.sujet = sujet;
-	}
 
-	public String getAuteur() {
-		return auteur;
-	}
+		public Date getDate() {
+			return date;
+		}
 
-	public void setAuteur(String auteur) {
-		this.auteur = auteur;
-	}
+		public void setDate(Date date) {
+			this.date = date;
+		}
 
-	public String getContenu() {
-		return contenu;
-	}
+		public String getFirstname() {
+			return firstname;
+		}
 
-	public void setContenu(String contenu) {
-		this.contenu = contenu;
-	}
+		public void setFirstname(String firstname) {
+			this.firstname = firstname;
+		}
 
-	public Message() {
-	}
+		public String getLastname() {
+			return lastname;
+		}
 
-	public Message(@NotBlank(message = "Sujet is mandatory") String sujet,
-			@NotBlank(message = "Auteur is mandatory") String auteur,
-			@NotBlank(message = "Reponse is mandatory") String reponse,
-			@NotBlank(message = "Contenu is mandatory") String contenu) {
-		super();
-		this.sujet = sujet;
-		this.auteur = auteur;
-		this.reponse = reponse;
-		this.contenu = contenu;
-	}
+		public void setLastname(String lastname) {
+			this.lastname = lastname;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public String getContenu() {
+			return contenu;
+		}
+
+		public void setContenu(String contenu) {
+			this.contenu = contenu;
+		}
+
+		public String getReponse() {
+			return reponse;
+		}
+
+		public void setReponse(String reponse) {
+			this.reponse = reponse;
+		}
+	    
+	   
 
 }
