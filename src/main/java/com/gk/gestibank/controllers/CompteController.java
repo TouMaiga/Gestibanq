6cc7843386c91813ea589dea4224558da89df81a
Corentin B <gta447@hotmail.fr>
2023-01-16 19:01:38 +0100

    last ver of CRUD compte - subject to modif.

--- src/main/java/com/gk/gestibank/controllers/CompteController.java
+++ src/main/java/com/gk/gestibank/controllers/CompteController.java
@@ -1,9 +1,19 @@
 package com.gk.gestibank.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.gk.gestibank.entities.Compte;
 import com.gk.gestibank.repositories.CompteRepository;
 
 @Controller
@@ -18,5 +28,71 @@ public class CompteController {
 			this.compteRepository = compteRepository;
 	}
 	
+	@GetMapping("list")
+    public String listComptes(Model model) {
+    	
+    	List<Compte> lp = (List<Compte>)compteRepository.findAll();
+    	long nbComptes =  compteRepository.count();
+    	if(lp.size() == 0) {
+    		
+    		lp = null;
+    	}
+        model.addAttribute("compte",lp);
+        model.addAttribute("nbComptes", nbComptes);
+      
+        return "compte/listComptes";
+        
+    }
+	
+	@GetMapping("add")
+    public String showAddCompteForm(Model model) {
+    	Compte compte = new Compte();// object dont la valeur des attributs par defaut
+    	model.addAttribute("agent", compte);
+        return "compte/addCompte";
+    }
+	
+	@PostMapping("add")
+    public String addCompte(@Valid Compte compte, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+        	System.out.println(result);
+            return "compte/addComptet";
+        }
+        
+       compteRepository.save(compte);
+        
+        return "redirect:list";
+    }
+	
+	@GetMapping("delete/{id}")
+    public String deleteCompte(@PathVariable("id") long id, Model model) {
+    	
+    	
+        Compte compte = compteRepository.findById(id)
+            .orElseThrow(()-> new IllegalArgumentException("Invalid compte Id:" + id));
+        
+        System.out.println("suite du programme...");
+        
+        compteRepository.delete(compte);
+        
+        return "redirect:../list";
+    }
+	
+	@GetMapping("edit/{id}")
+    public String showCompteFormToUpdate(@PathVariable("id") long id, Model model) {
+        Compte compte = compteRepository.findById(id)
+            .orElseThrow(()->new IllegalArgumentException("Invalid compte Id:" + id));
+        
+        model.addAttribute("compte", compte);
+        
+        return "compte/updateCompte";
+    }
+	
+	@PostMapping("update")
+    public String updateAgent(@Valid Compte compte, BindingResult result, Model model) {
+    	
+    	compteRepository.save(compte);
+    	return"redirect:list";
+    	
+    }
 	
 }
--- src/main/java/com/gk/gestibank/entities/Compte.java
+++ src/main/java/com/gk/gestibank/entities/Compte.java
@@ -26,6 +26,74 @@ public class Compte {
 	@Column(name = "email")
 	private String email;
 	
+	@NotBlank(message = "solde is mandatory")
+	@Column(name = "solde")
+	private double solde;
+	
+	@NotBlank(message = "numero_compte is mandatory")
+	@Column(name = "numero_compte")
+	private int numero_compte;
+	
+	@NotBlank(message = "type is mandatory")
+	@Column(name = "type")
+	private String type;
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	public int getNumero_compte() {
+		return numero_compte;
+	}
+
+	public void setNumero_compte(int numero_compte) {
+		this.numero_compte = numero_compte;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
 	
 }
