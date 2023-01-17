c489b7b2fc7bcd7765c702e8dbe4bf41d13eaba2
TouMaiga <maiga.maimouna@gmail.com>
2023-01-17 10:54:44 +0100

    intégration de client et agent

---
+++ src/main/java/com/gk/gestibank/controllers/CompteController.java
@@ -0,0 +1,98 @@
+package com.gk.gestibank.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gk.gestibank.entities.Compte;
+import com.gk.gestibank.repositories.CompteRepository;
+
+@Controller
+@RequestMapping("/compte/")
+public class CompteController {
+
+	private final CompteRepository compteRepository;
+	
+	@Autowired
+	public CompteController(CompteRepository compteRepository)
+	{
+			this.compteRepository = compteRepository;
+	}
+	
+	@GetMapping("list")
+    public String listComptes(Model model) {
+    	
+    	List<Compte> lp = (List<Compte>)compteRepository.findAll();
+    	long nbComptes =  compteRepository.count();
+    	if(lp.size() == 0) {
+    		
+    		lp = null;
+    	}
+        model.addAttribute("comptes",lp);
+        model.addAttribute("nbComptes", nbComptes);
+      
+        return "compte/listComptes";
+        
+    }
+	
+	@GetMapping("add")
+    public String showAddCompteForm(Model model) {
+    	Compte compte = new Compte();// object dont la valeur des attributs par defaut
+    	model.addAttribute("compte", compte);
+        return "compte/addCompte";
+    }
+	
+	@PostMapping("add")
+    public String addCompte(@Valid Compte compte, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+        	System.out.println(result);
+            return "compte/addCompte";
+        }
+        
+       compteRepository.save(compte);
+        
+        return "redirect:list";
+    }
+	
+	@GetMapping("delete/{id}")
+    public String deleteCompte(@PathVariable("id") long id, Model model) {
+    	
+    	
+        Compte compte = compteRepository.findById(id)
+            .orElseThrow(()-> new IllegalArgumentException("Invalid compte Id:" + id));
+        
+        System.out.println("suite du programme...");
+        
+        compteRepository.delete(compte);
+        
+        return "redirect:../list";
+    }
+	
+	@GetMapping("edit/{id}")
+    public String showCompteFormToUpdate(@PathVariable("id") long id, Model model) {
+        Compte compte = compteRepository.findById(id)
+            .orElseThrow(()->new IllegalArgumentException("Invalid compte Id:" + id));
+        
+        model.addAttribute("compte", compte);
+        
+        return "compte/updateCompte";
+    }
+	
+	@PostMapping("update")
+    public String updateAgent(@Valid Compte compte, BindingResult result, Model model) {
+    	
+    	compteRepository.save(compte);
+    	return"redirect:list";
+    	
+    }
+	
+}
---
+++ src/main/java/com/gk/gestibank/entities/Compte.java
@@ -0,0 +1,99 @@
+package com.gk.gestibank.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Compte {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@NotBlank(message = "Nom is mandatory")
+	@Column(name = "nom")
+	private String nom;
+
+	@NotBlank(message = "Prenom is mandatory")
+	@Column(name = "prenom")
+	private String prenom;
+
+	@NotBlank(message = "email is mandatory")
+	@Column(name = "email")
+	private String email;
+
+	@NotNull(message = "solde is mandatory")
+	@Column(name = "solde")
+	private double solde;
+
+	@NotNull(message = "numero_compte is mandatory")
+	@Column(name = "numero_compte")
+	private int numeroCompte;
+
+	@NotBlank(message = "type is mandatory")
+	@Column(name = "type")
+	private String type;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+
+	public int getNumeroCompte() {
+		return numeroCompte;
+	}
+
+	public void setNumeroCompte(int numeroCompte) {
+		this.numeroCompte = numeroCompte;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
